8cd8f529292c58408cc4ffc57c128d303ded61aa
johnoro <officialjohnorourke@gmail.com>
2022-10-18 13:21:20 -0400

    Initializes project

---
+++ be/src/test/java/com/johnoro/cpm/CpmApplicationTests.java
@@ -0,0 +1,10 @@
+package com.johnoro.cpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpmApplicationTests {
+	@Test
+	void contextLoads() { }
+}
